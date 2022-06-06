e19f3a040b092800da56a665061f755588410eba
marius.herciu23 <marius.herciu23@gmail.com>
2022-06-06 12:34:58 +0300

    nume si prenume separat

--- Program.cs
+++ Program.cs
@@ -18,35 +18,33 @@ namespace Ex.Suplimentar_Laborator5_
             Console.WriteLine("Introduceti numarul de elevi pe care doriti sa il introduceti in catalog:");
             int n = int.Parse(Console.ReadLine());
 
-            string[] numePrenume = new string[n * 2];
-            numePrenume = InroducereEleviCatalog(numePrenume);
+            string[] nume = new string[n];
+            string[] prenume = new string[n];
+            InroducereEleviCatalog(nume, prenume);
 
             double[][] noteElevi = new double[n][];
-            noteElevi = IntroducereNote(numePrenume, noteElevi);
+            noteElevi = IntroducereNote(nume, prenume, noteElevi);
 
-            string medieMare = AfisareElevMedieMax(numePrenume, noteElevi);
+            string medieMare = AfisareElevMedieMax(nume, prenume, noteElevi);
             Console.WriteLine(medieMare);
         }
-        static string[] InroducereEleviCatalog(string[] numePrenume)
+        static string[] InroducereEleviCatalog(string[] nume, string[] prenume)
         {
-            for (int i = 0; i < numePrenume.Length; i++)
+            for (int i = 0; i < nume.Length; i++)
             {
                 Console.WriteLine("Introduceti numele:");
-                numePrenume[i] = Console.ReadLine();
-                i++;
+                nume[i] = Console.ReadLine();
                 Console.WriteLine("Introduceti prenumele:");
-                numePrenume[i] = Console.ReadLine();
+                prenume[i] = Console.ReadLine();
             }
 
-            return numePrenume;
+            return nume;
         }
-        static double[][] IntroducereNote(string[] numePrenume, double[][] noteElevi)
+        static double[][] IntroducereNote(string[] nume, string[] prenume, double[][] noteElevi)
         {
-            int indexElevi = 0;
             for (int i = 0; i < noteElevi.Length; i++)
             {
-                string elevCurent = numePrenume[indexElevi] + " " + numePrenume[indexElevi + 1];
-                indexElevi += 2;
+                string elevCurent = nume[i] + " " + prenume[i];
                 Console.WriteLine($"Cate note introduceti pentru elevul {elevCurent}?", elevCurent);
                 int m = int.Parse(Console.ReadLine());
                 Console.WriteLine($"Introduceti {m} note:", m);
@@ -59,11 +57,10 @@ namespace Ex.Suplimentar_Laborator5_
 
             return noteElevi;
         }
-        static string AfisareElevMedieMax(string[] numePrenume, double[][] noteElevi)
+        static string AfisareElevMedieMax(string[] nume, string[] prenume, double[][] noteElevi)
         {
             string rezultat = String.Empty;
             double medieMare = 0;
-            int indexElevi = 0;
             for (int i = 0; i < noteElevi.Length; i++)
             {
                 double medieCurenta = 0;
@@ -79,9 +76,8 @@ namespace Ex.Suplimentar_Laborator5_
                 {
                     medieMare = medieCurenta;
                     rezultat = String.Empty;
-                    rezultat = "Elevul " + numePrenume[indexElevi] + " " + numePrenume[indexElevi + 1] + " a avut cea mai mare medie, respectiv " + medieMare.ToString("n2") + ".";
+                    rezultat = ($"Elevul {nume[i]} {prenume[i]} a avut cea mai mare medie, respectiv {medieMare.ToString("n2")}.").ToString();
                 }
-                indexElevi += 2;
             }
 
             return rezultat;
